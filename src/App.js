020fd00fbb22d0ac680f7d3be45291fe95408f24
Lirico <dominguezmatiasadrian@gmail.com>
2020-11-08 19:01:01 -0300

    version 3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Fragment } from 'react';
 import Header from './components/Header';
 import Formulario from './components/Formulario'
 
+import CategoriasProvider from './context/CategoriasContext';
+import RecetasProvider from './context/RecetasContext';
+
 function App() {
+
   return (
-      <Fragment>
-        <Header />  
+      <CategoriasProvider>
+        <RecetasProvider>
+          <Header />  
 
-        <div className="container mt-5">
-          <div className="row">
-            <Formulario />
+          <div className="container mt-5">
+            <div className="row">
+              <Formulario />
+            </div>
           </div>
-        </div>
-
-      </Fragment>
+        </RecetasProvider>
+      </CategoriasProvider>
   );
 }
 
--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { CategoriasContext } from '../context/CategoriasContext';
+import { RecetasContext } from '../context/RecetasContext';
 
 const Formulario = () => {
+
+    const [ busqueda, guardarBusqueda ] = useState({
+        nombre: '',
+        categoria: ''
+    });
+
+    const { categorias } = useContext(CategoriasContext);
+    const { buscarRecetas }  = useContext(RecetasContext);
+;
+    // funcion para leer los contenidos
+    const obtenerDatosReceta = e => {
+        guardarBusqueda({
+            ...busqueda,
+            [e.target.name] : e.target.value
+        });
+    }
+
     return ( 
         <form
             className="col-12"
+            onSubmit={e => {
+                e.preventDefault();
+                buscarRecetas(busqueda)
+            }}
         >
             <fieldset className="text-center">
                 <legend>Busca bebidas por Categoría o Ingredientes</legend>
@@ -15,18 +38,26 @@ const Formulario = () => {
                         name="nombre"
                         className="form-control"
                         type="text"
-                        placeholder="Buscar por Ingrediente" 
+                        placeholder="Buscar por Ingrediente"
+                        onChange={obtenerDatosReceta} 
                     />
                 </div>
                 <div className="col md-4">
                     <select
                         name="categoria"
                         className="form-control"
+                        onChange={obtenerDatosReceta}
                     >
                         <option value="">-- Selecciona Categoría --</option>
+                        {categorias.map(categoria => (
+                            <option
+                                key={categoria.strCategory}
+                                value={categoria.strCategory}
+                            >{categoria.strCategory}</option>
+                        ))}
                     </select>
                 </div>
-                <div class="col md-4">
+                <div className="col md-4">
                     <button
                         type="submit"
                         className="btn btn-primary btn-block"
